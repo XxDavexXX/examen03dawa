6ec705985999e91b1aaff8c677511d49309e16e8
XxDavexX <dave.santivanez@gmail.com>
2024-05-27 23:21:17 -0500

    avance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { MarcasComponent } from './components/marcas/marcas.component';
+import { ContactenosComponent } from './components/contactenos/contactenos.component';
+import { NavmenuComponent } from './components/navmenu/navmenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NosotrosComponent,
+    ServiciosComponent,
+    InicioComponent,
+    ProductosComponent,
+    MarcasComponent,
+    ContactenosComponent,
+    NavmenuComponent
   ],
   imports: [
     BrowserModule,
